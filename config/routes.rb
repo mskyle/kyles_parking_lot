7b092fd1edf3b9365c58fb5e3453dd65b4b4898f
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-09-16 13:14:27 -0400

    Parking registration feature User can register a parking space with their name and email and the space number

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 KylesParkingLot::Application.routes.draw do
+  resources :registrations
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'registrations#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
