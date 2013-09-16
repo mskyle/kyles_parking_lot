408ca1e3cdb0dfcc8d605f21c7b80cb51741b5b3
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-09-16 08:51:27 -0400

    rspec installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KylesParkingLot::Application.config.secret_key_base = 'b58ec7d1401290848abf9b7512f708dbe325a57fc587f218885ab8c2941285cde9686ac77ebbdc0f1deb8abe559419ccb1121100fa25809ddd93c670662465f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KylesParkingLot::Application.config.session_store :cookie_store, key: '_kyles_parking_lot_session'
