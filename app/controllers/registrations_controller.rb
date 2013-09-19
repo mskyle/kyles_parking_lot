4e2f0f5f4548e4f5f8a8b8d5039fc34b55b0930c
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-09-18 21:08:23 -0400

    got the session memory email fill-in thing working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,13 @@ class RegistrationsController < ApplicationController
 
   def new
     @registration = Registration.new
+    @last_registration_id = session[:last_registration_id]
   end
 
   def create
     @registration = Registration.new(reg_params)
     if @registration.park
+      session[:last_registration_id] = @registration.id
       flash[:notice] = "You registered successfully. #{@registration.neighbor_message}"
       redirect_to '/'
     else
